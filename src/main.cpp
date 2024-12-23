e3d8747ce372693dd7b89c963f8c8c1d0d02a1c9
Kris Bellemans <kris.bellemans@technomatic.be>
2024-12-23 19:53:24 +0100

    Add example project

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "utils.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from MyProject!" << std::endl;
+    print_utility_message();
+    return 0;
+
---
+++ src/utils.cpp
@@ -0,0 +1,6 @@
+#include "utils.hpp"
+#include <iostream>
+
+void print_utility_message() {
+    std::cout << "Utility function executed!" << std::endl;
+}
